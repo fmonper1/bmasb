0cdc57f66bb0d6d94f177d3be3ce9cd597b89009
Fernando Montero <fmonteroperez1@sheffield.ac.uk>
2017-12-19 20:56:43 +0000

    migrating to psql

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'rails', '~> 5.1.2'
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3', group: [:development, :test]
 gem 'pg'
+gem 'seed_dump'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {email: "fernando.montero.perez@gmail.com", encrypted_password: "$2a$11$dLvnNVXjmUNvGcgqRMwkgeFne5EI6oHNQ7HALef8qsJ.aRq/WqHLu", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2017-12-19 20:35:22", last_sign_in_at: "2017-12-19 20:35:22", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
+])
+Company.create!([
+  {titulo: "New Balance Canarias"}
+])
+Shop.create!([
+  {titulo: "New Balance La Laguna", dir1: "", dir2: "", dir3: "", dir4: "", company_id: 1}
+])
