44e40ebed5e96f0019826ca2df29832547121df2
Fernando Montero Pérez <fernando.montero.perez@gmail.com>
2017-12-20 03:09:02 +0000

    d

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "events#index"
 
+  as :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+  
   get '/events/completados', to: 'events#scompletados'
 
 end
